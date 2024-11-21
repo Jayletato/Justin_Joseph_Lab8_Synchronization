7a1ceec5df4b9185bf64c86a6083230aaf486d9d
jayletato <jayjtrainer@gmail.com>
2024-11-21 06:49:36 +0000

    tried implementing multithread

--- problem2.c
+++ problem2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
+#define NUM_THREADS 1
 
 int fibonacci(int num) {
   if (num <= 1 ) {
@@ -13,34 +15,50 @@ int fibonacci(int num) {
 }
 
 long * fibSequence;
+long num;
+pthread_mutex_t lock;
 
-long *fibonacciArray(long num) {
-  long *fibArray = (long*)malloc(num * sizeof(long));
-  if (fibArray == NULL) {
+void *fibonacciArray(void *arg) {
+  pthread_mutex_lock(&lock);
+
+  long * fibArray = (long*)malloc(num * sizeof(long));
+  if (fibSequence == NULL) {
     exit(1);
     }
 
   for (int i = 0; i < num; i++) {
-    fibArray[i] = fibonacci(i);
+    fibSequence[i] = fibonacci(i);
     }
-
-  return fibArray;
+  
+  pthread_mutex_unlock(&lock);
 }
 
 int main(int argc, char *argv[]) {
+
+  pthread_t threads[NUM_THREADS];
+  pthread_mutex_init(&lock, NULL);
+
   if (argc != 2) {
     printf("This program only takes one argument!\n");
     exit(1);
   }
   char *p;
-  long num = strtol(argv[1], &p, 10);
-  
-  fibSequence = fibonacciArray(num);
+  num = strtol(argv[1], &p, 10);
+
+  for (int i = 0; i < NUM_THREADS; i++){
+    pthread_create(&threads[i], NULL, fibonacciArray, NULL);
+  }
 
   for (int i = 0; i < num; i++) {
     printf("%ld ", fibSequence[i]);
-    }
+  }
+
   printf("\n");
 
+  for (int i = 0; i < NUM_THREADS; i++){
+    pthread_join(threads[i], NULL);
+  }
+
+  pthread_mutex_destroy(&lock);
   free(fibSequence);
 }
